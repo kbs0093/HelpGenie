16624534c597006d48d0d72cadf19b9f17936a9c
rhqjatjr0093 <rhqjatjr0093@gmail.com>
2021-06-30 08:03:54 +0900

    비디오 저장 / 웹 캠 세팅으로 변경

--- CameraStream.py
+++ CameraStream.py
@@ -1,61 +1,19 @@
-from threading import Thread
 import cv2
-import time
 
-class VideoScreenshot(object):
-    def __init__(self, src=0):
-        # Create a VideoCapture object
-        self.capture = cv2.VideoCapture(src)
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('test.avi',fourcc, 20.0, (640,480))
 
-        # Take screenshot every x seconds
-        self.screenshot_interval = 1
 
-        # Default resolutions of the frame are obtained (system dependent)
-        self.frame_width = int(self.capture.get(3))
-        self.frame_height = int(self.capture.get(4))
+def recordVideo(fname):
+    
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        out.write(frame)
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-        # Start the thread to read frames from the video stream
-        self.thread = Thread(target=self.update, args=())
-        self.thread.daemon = True
-        self.thread.start()
-
-    def update(self):
-        # Read the next frame from the stream in a different thread
-        while True:
-            if self.capture.isOpened():
-                (self.status, self.frame) = self.capture.read()
-
-    def show_frame(self):
-        # Display frames in main program
-        if self.status:
-            cv2.imshow('frame', self.frame)
-
-        # Press Q on keyboard to stop recording
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            self.capture.release()
-            cv2.destroyAllWindows()
-            exit(1)
-
-    def save_frame(self):
-        # Save obtained frame periodically
-        self.frame_count = 0
-        def save_frame_thread():
-            while True:
-                try:
-                    cv2.imwrite('frame_{}.png'.format(self.frame_count), self.frame)
-                    self.frame_count += 1
-                    time.sleep(self.screenshot_interval)
-                except AttributeError:
-                    pass
-        Thread(target=save_frame_thread, args=()).start()
-
-def playWebCam()::
-    rtsp_stream_link = 'rtsp://helpgenie:k5336309@192.168.25.23:554/stream1'
-    video_stream_widget = VideoScreenshot(rtsp_stream_link)
-    video_stream_widget.save_frame()
-    while True:
-        try:
-            video_stream_widget.show_frame()
-        except AttributeError:
-            pass
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
