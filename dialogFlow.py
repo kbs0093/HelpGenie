022055255f3c94e597ae2ad1624ddd44fe07eb35
rhqjatjr0093 <rhqjatjr0093@gmail.com>
2021-07-09 11:19:25 +0900

    comment

--- dialogFlow.py
+++ dialogFlow.py
@@ -3,11 +3,12 @@ import os
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./kt-helpgenie-mqjf-888ca127377d.json"
 
+# 문장을 자연어 처리하여 핵심 키워드로 추출하는 함수
 def detect_intent_texts(texts):
     if (texts == "" or texts is None):
         return ""
         
-
+    # 구글 다이얼로그 플로우 프로젝트 경로
     project_id = 'kt-helpgenie-mqjf'
     session_id = '123456789'
     language_code = 'ko'
@@ -16,7 +17,7 @@ def detect_intent_texts(texts):
     session = session_client.session_path(project_id, session_id)
     #print("Session path: {}\n".format(session))
 
-    
+    # 자연어 처리
     text_input = dialogflow.TextInput(text=texts, language_code=language_code)
 
     query_input = dialogflow.QueryInput(text=text_input)
@@ -24,17 +25,5 @@ def detect_intent_texts(texts):
     response = session_client.detect_intent(
         request={"session": session, "query_input": query_input}
     )
-
-    '''
-    print("=" * 20)
-    print("Query text: {}".format(response.query_result.query_text))
-    print(
-        "Detected intent: {} (confidence: {})\n".format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence,
-        )
-    )
-    print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
-    '''
     
     return response.query_result.fulfillment_text
